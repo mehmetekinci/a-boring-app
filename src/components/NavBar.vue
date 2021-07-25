f23bdfc061f99e51a274ddc7a9fee37b465330de
mehmetekinci <mehmetekinci1989@gmail.com>
2021-07-25 07:34:39 +0200

    tailwind library config, home page creation,Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="
+      h-20
+      flex
+      items-end
+      space-x-7
+      ml
+      text-purple-500
+      rounded-t-xl
+      bg-white
+      text-lg
+      font-bold
+      shadow-md
+    "
+  >
+    <router-link
+      class="ml-2 px-2 py-1 hover:bg-purple-500 hover:text-pink-50 rounded-t-xl"
+      to="/"
+      >Home</router-link
+    >
+    <router-link
+      class="px-2 py-1 hover:bg-purple-500 hover:text-pink-50 rounded-t-xl"
+      to="/advice"
+      >Do you need some help?</router-link
+    >
+
+    <router-link
+      class="px-2 py-1 hover:bg-purple-500 hover:text-pink-50 rounded-t-xl"
+      to="/bookmarks"
+      >Bookmarks</router-link
+    >
+  </div>
+</template>
---
+++ src/views/Bookmarks.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bookmarks">
+    <h1>This is an bookmarks page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+</style>
